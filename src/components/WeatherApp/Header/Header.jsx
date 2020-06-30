5738e5aa96b65b1a4889747f80fed8ed6842f4c2
Max Cramer <maximiliancramer@yahoo.com>
2020-06-30 16:53:37 +0100

    thinking about how to filter days so only one of each day displays, adding more styling

--- src/components/WeatherApp/Header/Header.jsx
+++ src/components/WeatherApp/Header/Header.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { withRouter } from 'react-router';
+// import { render } from '@testing-library/react';
 
 function Header() {
-    return(
-        <h2>Header Component</h2>
+    const [headerItems, setHeaderItems] = useState([]);
+    
+    async function loadData() {
+        const url = 'api.openweathermap.org/data/2.5/weather?q=london&appid=ac9d46375bac97fb9ce96c7ffdb4851d';
+        const response = await fetch(url);
+        const data = await response.json(response);
+        const headerData = data;
+        setHeaderItems(headerData);
+        console.log(data);
+        // console.log("this is the city data", data.name);
+    }
+
+ useEffect(() => {
+   loadData();
+ }, []);
+
+
+    return (
+        <div>
+        <h2>header</h2>
+            {
+                headerItems.map(h => (
+                    <p>{h.name}</p>
+                ))
+            }
+        </div>
     )
 }
 
-export default Header;
+export default withRouter(Header);
