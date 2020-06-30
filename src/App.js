006e5939cc0ed24ee484c57fcdba34257ac9a1f4
Max Cramer <maximiliancramer@yahoo.com>
2020-06-30 11:47:57 +0100

    setting up app structure,

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import WeatherApp from '../src/components/WeatherApp'
+import WeatherApp from './components/WeatherApp/WeatherApp'
 
 import './App.css';
 
---
+++ src/components/WeatherApp/Body/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Body() {
+    return(
+        <div>
+            <h2>Body</h2>
+        </div>
+    );
+}
+
+export default Body;
