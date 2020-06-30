4a8f93c4b4d54226bc188ce9cdbccc63ab2d391b
Max Cramer <maximiliancramer@yahoo.com>
2020-06-30 12:44:22 +0100

    data console loggin in promoise

--- src/components/WeatherApp/Body/Body.jsx
+++ src/components/WeatherApp/Body/Body.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
-function Body() {
-    return(
-        <div>
-            <h2>Body</h2>
-        </div>
+class Body extends React.Component {
+  state = {
+    loading: true
+  };
+
+  
+  async componentDidMount() {
+    const url = 'http://api.openweathermap.org/data/2.5/forecast?q=london&appid=ac9d46375bac97fb9ce96c7ffdb4851d';
+    const response = await fetch(url);
+    const data = response.json(response);
+    console.log("this is the data", data);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>body</h2>
+      </div>
     );
+  }
 }
 
 export default Body;
